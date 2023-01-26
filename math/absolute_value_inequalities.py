1e5d58c7328fbdb042f38bc45d8cf19c1b3c30a6
Kevin <Kevin@macusers-MacBook-Pro-5.local>
2023-01-26 11:18:32 -0500

    notes

---
+++ math/absolute_value_inequalities.py
@@ -0,0 +1,18 @@
+'''
+absolute value |u|=a which means u can be = a or -a
+|3| = 3 and |-3| = 3. It's just the distance from 0.
+
+|3x|=9
+which is 3x = 9 or 3x = -9
+so x = 3 or x = -3
+
+
+|4 * x + 2| = 6
+x = 1 or x = -2
+
+
+|5 * x - 1| = 4
+5 * x - 1 = 4 or 5 * x - 1 = -4
++ 1
+5x = 5 or x = -3
+'''
--- math/inequalities.py
+++ math/inequalities.py
@@ -1,4 +1,7 @@
+from sympy import *
+
 '''
+set notation
 inequality  | numberline        | interval
 x < a       | -oo <===0===a---> | (-oo, a)
                 all numbers < a
@@ -12,3 +15,36 @@ a <= x < b  | <--[==0==b-->     | [a, b)
 
 a>b or x<b  | -oo <==b---a==> oo| (-oo, b) or (a, 00)
 '''
+
+'''
+examples
+#1, x + 8 < 11
+#2, 2 - 6 * x <= -4
+#3, 7 * x - 11 <= 9 + 2 * x
+#4, 6 - 3 * (1 - x) <= 5
+(-oo, 2/3] or x <= 2/3
+#5, 1/5 * (x - 12) > x + 16
+(-oo, -23) or x < -23
+'''
+
+
+x = symbols('x')
+
+print(
+    reduce_inequalities(1/5 * (x - 8) > x + 16, [])
+)
+'''
+'''
+
+
+
+'''
+compounded inequality
+4 < x - 8 < 10
+'''
+
+print(
+    solve(
+        4 < x - 8
+    )
+)
