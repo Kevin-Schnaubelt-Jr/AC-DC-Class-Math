3a685f2e20520ddf094e87a33aa1c1c9376dc5ec
Kevin <Kevin@macusers-MacBook-Pro-5.local>
2023-02-28 08:53:50 -0500

    prop of funcs

--- math/functions.py
+++ math/functions.py
@@ -65,6 +65,62 @@ def function_1(x):
         x = symbols('x')
         return solve(3 * x**2 + 2 * x - 4)
 
-print(
-    function_1(input('>: '))
-)
+# print(
+#     function_1(input('>: '))
+# )
+
+'''
+find the domain
+
+example 1: x as a fraction
+f(x) = (2*x) / (x**2-25)
+
+example 2:
+y = 3*x**2+4*x-2
+
+example 3:
+g(x) = sqrt(-2*x-4)
+'''
+
+'''
+example 1:
+dominator is x**2-25 = 0
+so add 25 to both sides, then sqrt both sides
+so x**2 = 25
+x = +- 5
+
+so domain is all x but ! -5,5
+(-oo,-5)U(-5,5)U(5,oo)
+'''
+'''
+example 2:
+(-oo,oo) because no fractions or sqrt
+'''
+'''
+example 3:
+so set -2*x-4 >= 0
+so add 4.
+-2*x>=4
+so divide -2 divide or multiply by a negative flop the inequality
+x >= -2
+(-oo,-2]
+'''
+
+'''
+example 4: find domain
+p(t) = (sqrt(t-6)) / (3*t-24)
+
+case 1 denominator 3*t-24=0
+add 24
+3*t=24
+divide 3
+t = 8
+so 8 is excluded by the domain.
+
+case 2 squart root
+set t - 6 >= 0
+add 6
+t >= 6
+so domain is everything great than or equal to 6, but not 8
+[6,8)U(8,oo)
+'''
---
+++ math/properties_of_funcions.py
@@ -0,0 +1,28 @@
+from sympy import *
+
+'''
+topics
+1) vertical line test
+2) figuring out domain and range and intercepts from a graph
+3) even and odd functions from graphing.
+4) increasing, decreasing, and constant
+5) finding max and min
+'''
+
+'''
+vertical "VERTICAL" line test. see if graph is a function
+
+VERTICAL line that intersects once is a function. 
+more than once, not a function
+'''
+
+'''
+domain, range, intercepts from graph
+domain is all the points in between and including that farthest x-axis points.
+the range is all the points in between and including the farthest y-axis points
+just the points with 0 for x or y.
+
+with arrows, things are different. since arrows pointing down south west and south east, all x values are -oo to oo.
+so domain is (-oo,oo)
+for the range, since the arrows are only pointing down, it's (-oo, highest y point]
+'''
