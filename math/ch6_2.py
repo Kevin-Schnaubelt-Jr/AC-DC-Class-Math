d3f07626ccf2bf31900619af5865ac9c5600d690
Kevin <Kevin@macusers-MacBook-Pro-5.local>
2023-03-28 08:52:19 -0400

    ch6 2

---
+++ math/ch6_2.py
@@ -0,0 +1,28 @@
+from sympy import *
+
+'''
+topics
+'''
+
+'''
+exponent rules:
+1) a1 = a
+2) a**0 = 1, but 0**0 is undefined
+3) am/an = a*m-n
+4) a**m * a**n = a*m+n
+5) (1/b)**n = (a**n)/(b**n)
+6) (a/b)**-n = (b/a)**n
+7) a**-n = 1/(a**n)
+8) (a**m)**n = a**m*n
+9) a**u = a**v
+then u=v
+'''
+
+'''
+examples:
+2**-x = 4
+(3/2)**x = 27/8
+2**(2x+1) = 4
+
+for the first one, make 4 into 2**2. then you can do 
+'''
