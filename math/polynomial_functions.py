5d95741c388dd557cde19fc6cc5d498fca9b23b9
Kevin <Kevin@macusers-MacBook-Pro-5.local>
2023-04-11 12:57:31 -0400

    poly funk

---
+++ math/polynomial_functions.py
@@ -0,0 +1,102 @@
+'''
+some examples of polynomial_functions are:
+f(x) = 2*x**2 + x - 2
+g(x) = (1/2)*x**3 - x
+h(x) = sqrt(2*x**4) - 2*x**4 + 1
+
+all of these consist of terms such as 2*x**2 and their separated by a + or -.
+They all have some variable x. 
+Exponents for the variables are positive whole numbers.
+coefficients are stuff attached to the X. So for 1, x  has a coefficient of 2, and the second x has a coefficient of 1. The -2 on its own
+is the constant.
+
+The graphs look generally like smooth and continuous curves.
+like a parabola or a sine wave. peaks are round, unlike absolute value graphs, which are like "V".
+
+'''
+'''
+standard form
+f(x) = a_n*x**n  + ... + a_2*x**2 + a_1*x + a_0
+a_n subscript of n a_n*x**h is the nth term.
+all the a's are just numbers.
+a_n x**h is the leading term (has the highest exponent for x) a_n is leading coefficient.
+
+so for f(x) = 2*x**2 + x - 2:
+the leading term is 2*x**2
+the leading coefficient is 2
+and the constant term is -2.
+the degree is the highest exponent, which is 2 in this case.
+'''
+
+'''
+example: y = 2 - x**5 + x
+lets rewrite it
+y = -x**5 + x + 2
+now we can see it's a polynomial.
+the leading term is -x**5
+the constant term is 2
+and the degree is 5
+'''
+
+'''
+example 2:
+f(x) = x**(5/2) - x**4 + 4
+this is NOT a polynomial because it has an exponent that isn't a positive whole number.
+the variable can also not be in the denominator. 
+'''
+
+'''
+example 3:
+y = (9 - x**4) / 4
+you can rewrite this into standard form by doing ((-x**4) / 4) + (9/4)
+the leading term is -x**4/4
+constant 9/4
+'''
+
+'''
+a poly graph looks like a snap. "U^".
+it has 3 intercepts. (zeros)
+degree: 3, which matches the x intercepts usually.
+the degree tells us the max numbers of x intercepts, or zeros.
+
+so for 3 points, -6, -1, 7. or (-6,0), (-1,0), (7,0)
+
+The leading term is going to trump all over terms in how it's graphed.
+in particular it will resemble a_n*x**n
+f(x) = 3*x**2 - x + 1
+the leading term, 3*x**2, is going to be a parabola "U".
+2 is the degree, meaning the max numbers of zeros is 2.
+'''
+
+'''
+zeros of polynomial
+
+example:
+f(x) = 2*x**3 + 3*x**2 + 2*x - 5
+determine eif x-1 is a factor. If it is, that means x=1 is a zero.
+so if we plug 1 into the function to see if the output is zero.
+it's 2, so x is NOT a factor.
+'''
+
+'''
+example 2:
+f(x) = 2*x**4 - 4*x**3 - 4*x + 6
+so divide by x-1, which is x=1
+so plug 1 into the function, and we get 0, so it is a factor.
+'''
+
+'''
+Descartes's rule of sign.
+f(x) = 6*x**7 + x**3 - x**2 + 5
+degree of 7.  ^------^ sign change.
+also - x**2 + 5 is another sign change.
+so 2 sign changes, so we expect either 2 or 2 less numbers (0) of positives zeros.
+
+for negative zeros, but in (-x) in to the function.
+
+y = 6*x**6 - 4*x**2 - x + 7
+6 is the degree, so the max numbers of zeros is 6.
+6 to -6 is a sign change, then a -x to + 7 is a sign change, so 2 sign changes.
+so 2 or no positive zeros.
+how many negative zeros? plug in -x. there's 2 sign changes in there, so 2 or zero negative zeros.
+'''
