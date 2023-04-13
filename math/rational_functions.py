c444f39399fd8b76785e7996a10dae42f5990da1
Kevin <Kevin@macusers-MacBook-Pro-5.local>
2023-04-13 08:59:01 -0400

    rational fun init

---
+++ math/rational_functions.py
@@ -0,0 +1,15 @@
+'''
+Rational function is a fraction where the top and bottom are polynomials.
+Graphs look similar to 
+
+dashy line up, dashy line down, and then 2 exponential lines mirrored across "origins"
+There's both a vertical and horizontal asymptote.
+
+for the vertical "VA":
+so for example x = 7, as x approaches 7 from the RIGHT, f(x) goes to oo.
+as x approaching 7 from the LEFT f(x) goes to -oo
+
+for the horizontal "HA":
+so for y = 5
+as x goes forever to the left, f(x) approaches 5.
+'''
